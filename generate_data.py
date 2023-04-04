1b2f7732a16e3f581115982b0aa122e4141570f2
Son Anh <sonanhbui2407@gmail.com>
2023-04-05 02:28:32 +0700

    ok

--- generate_data.py
+++ generate_data.py
@@ -1,10 +1,14 @@
 import random
+import sys
 
 numbers = []
-while len(numbers) < 100:
-    num_str = str(random.randint(1, 99999)).zfill(5)
-    if num_str not in numbers:
-        numbers.append(num_str)
+for i in range(4000):
+    while True:
+        num = random.sample(range(10), 5)
+        if not any(num.count(x) > 1 for x in num):
+            numbers.append("'" + ''.join(map(str, num)) + "',")
+            break
 
-# Print the list of numbers
-print(numbers)
+with open("numbers.txt", "w") as f:
+    sys.stdout = f
+    print(*numbers, sep='\n')
